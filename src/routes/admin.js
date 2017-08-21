00f84471c01e8fb0987d6e0d12ad9ab742972652
Dave Cozens <davecozens.defra@gmail.com>
2017-08-21 13:40:10 +0100

    updated readme

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,10 +3,10 @@ const Admin= require('../lib/admin')
 
 module.exports = [
 
-  { method: 'GET', path: '/admin', handler: Admin.index },
-  { method: 'GET', path: '/admin/fields', handler: Admin.fields },
-  { method: 'GET', path: '/admin/organisation', handler: Admin.organisations },
-  { method: 'GET', path: '/admin/organisation/{orgId}/licencetypes', handler: Admin.organisationLicenceTypes },
-  { method: 'GET', path: '/admin/organisation/{orgId}/licencetypes/{typeId}', handler: Admin.organisationLicenceType },
-  { method: 'POST', path: '/admin/organisation/{orgId}/licencetypes/{typeId}/field', handler: Admin.addFieldToOrganisationLicenceType }
+  { method: 'GET', path: '/admin', config:{ auth: false }, handler: Admin.index },
+  { method: 'GET', path: '/admin/fields', config:{ auth: false }, handler: Admin.fields },
+  { method: 'GET', path: '/admin/organisation', config:{ auth: false }, handler: Admin.organisations },
+  { method: 'GET', path: '/admin/organisation/{orgId}/licencetypes', config:{ auth: false }, handler: Admin.organisationLicenceTypes },
+  { method: 'GET', path: '/admin/organisation/{orgId}/licencetypes/{typeId}', config:{ auth: false }, handler: Admin.organisationLicenceType },
+  { method: 'POST', path: '/admin/organisation/{orgId}/licencetypes/{typeId}/field', config:{ auth: false }, handler: Admin.addFieldToOrganisationLicenceType }
 ]

01e9905a70a67a6a1a3bb7589d223e18bfa0bf3c
Dave Cozens <davecozens.defra@gmail.com>
2017-09-08 08:13:16 +0100

    change route for shortcode use

--- src/views/index.js
+++ src/views/index.js
@@ -82,10 +82,10 @@ const defaultContext = {
 
 
 
-  propositionHeader: '<div class="header-proposition"><div class="content"><nav id="proposition-menu"><a href="/" id="proposition-name">Water resource licensing service</a></nav></div></div>',
+  propositionHeader: '<div class="header-proposition"><div class="content"><nav id="proposition-menu"><a href="/" id="proposition-name">Water resource licensing admin service</a></nav></div></div>',
   afterHeader: '',
   footerTop: '',
-  footerSupportLinks: '<ul><li><a href="/">Clear data</a></ul>',
+  footerSupportLinks: '<ul><li><a href="/logout">Log Out</a></li></ul>',
   licenceMessage: '<p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated</p>',
   bodyEnd: ''
 }

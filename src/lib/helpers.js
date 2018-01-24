2899dd3cc2645c6e313d6fbd096b5c87b1742bb2
James Carmichael <james@siteclick.co.uk>
2018-01-24 10:36:39 +0000

    Update helpers.js

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -7,7 +7,7 @@ var bcrypt = require('bcrypt');
  * @return {String} - with references in form AB 123 123
  */
 function reduceGridReferenceResolution(str) {
-  const r = /(HO|HP|HT|HU|HW|HX|HY|HZ|NA|NB|NC|ND|NE|NF|NG|NH|NJ|NK|NL|NM|NN|NO|NP|NR|NS|NT|NU|NW|NX|NY|NZ|OV|SC|SD|SE|TA|SH|SJ|SK|TF|TG|SM|SN|SO|SP|TL|TM|SR|SS|ST|SU|TQ|TR|SV|SW|SX|SY|SZ|TV) ?([0-9]{4}) ?([0-9]{4})/g;
+  const r = /(HO|HP|HT|HU|HW|HX|HY|HZ|NA|NB|NC|ND|NE|NF|NG|NH|NJ|NK|NL|NM|NN|NO|NP|NR|NS|NT|NU|NW|NX|NY|NZ|OV|SC|SD|SE|TA|SH|SJ|SK|TF|TG|SM|SN|SO|SP|TL|TM|SR|SS|ST|SU|TQ|TR|SV|SW|SX|SY|SZ|TV) ?([0-9]{3,5}) ?([0-9]{3,5})/g;
   let match;
   while((match = r.exec(str)) !== null) {
     const newRef = `${ match[1] } ${ match[2].substr(0, 3) } ${ match[3].substr(0, 3) }`;

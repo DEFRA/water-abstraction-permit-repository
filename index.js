45acffca6fef8fa9e4d6264ca3edfbeebc487943
James Carmichael <james@siteclick.co.uk>
2018-04-06 11:33:54 +0100

    Don't specify host name

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-//provides permit API
-require('dotenv').config()
+// provides permit API
+require('dotenv').config();
 
-const Hapi = require('hapi')
+const Hapi = require('hapi');
 
 // create new server instance and connection information
 const server = new Hapi.Server({
-  host: 'localhost',
   port: process.env.PORT,
   router: {stripTrailingSlash: true}
 });
@@ -13,12 +12,12 @@ const server = new Hapi.Server({
 // server.connection({ port: process.env.PORT || 8000 })
 
 if (process.env.DATABASE_URL) {
-  var workingVariable = process.env.DATABASE_URL.replace('postgres://', '')
-  process.env.PGUSER = workingVariable.split('@')[0].split(':')[0]
-  process.env.PGPASSWORD = workingVariable.split('@')[0].split(':')[1]
-  process.env.PGHOST = workingVariable.split('@')[1].split(':')[0]
-  process.env.PSPORT = workingVariable.split('@')[1].split(':')[1].split('/')[0]
-  process.env.PGDATABASE = workingVariable.split('@')[1].split(':')[1].split('/')[1]
+  var workingVariable = process.env.DATABASE_URL.replace('postgres://', '');
+  process.env.PGUSER = workingVariable.split('@')[0].split(':')[0];
+  process.env.PGPASSWORD = workingVariable.split('@')[0].split(':')[1];
+  process.env.PGHOST = workingVariable.split('@')[1].split(':')[0];
+  process.env.PSPORT = workingVariable.split('@')[1].split(':')[1].split('/')[0];
+  process.env.PGDATABASE = workingVariable.split('@')[1].split(':')[1].split('/')[1];
 }
 
 /**
@@ -27,22 +26,20 @@ if (process.env.DATABASE_URL) {
  * @param {Object} request - current request
  * @return {Object} - result
  */
-async function validate(decoded, request) {
-    if (!decoded.id) {
-      console.log('boo... JWT failed')
-      return { isValid: false }
-    } else {
-      console.log('huzah... JWT OK')
-      return { isValid: true }
-    }
+async function validate (decoded, request) {
+  if (!decoded.id) {
+    console.log('boo... JWT failed');
+    return { isValid: false };
+  } else {
+    console.log('huzah... JWT OK');
+    return { isValid: true };
   }
-
+}
 
 /**
  * Async function to start HAPI server
  */
-async function start() {
-
+async function start () {
   // Node HAPI airbrake plugin
   await server.register({
     plugin: require('node-hapi-airbrake'),
@@ -66,7 +63,7 @@ async function start() {
   server.auth.strategy('jwt', 'jwt',
     { key: process.env.JWT_SECRET,
       validate
-    })
+    });
 
   server.auth.default('jwt');
 
@@ -75,11 +72,11 @@ async function start() {
 
   await server.start();
 
-  console.log(`Server started on port ${ process.env.PORT }`);
+  console.log(`Server started on port ${process.env.PORT}`);
 
   return server;
 }
 
 start();
 
-module.exports = server
+module.exports = server;

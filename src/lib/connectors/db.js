3dcd54cee36eb262010cf777e97beaea2cf9ad85
Mark Swaffer <mdswaffer@gmail.com>
2018-07-23 14:57:05 +0100

    Adds connection pool logging
    - Shares the same connection pool across all database access.
    - Logs to the console if there are requests waiting for a free
    connection from the connection pool.

--- src/lib/connectors/db.js
+++ src/lib/connectors/db.js
@@ -3,6 +3,13 @@ const { Pool } = require('pg');
 
 const pool = new Pool(pg);
 
+pool.on('acquire', () => {
+  const { totalCount, idleCount, waitingCount } = pool;
+  if (totalCount === pg.max && idleCount === 0 && waitingCount > 0) {
+    console.log(`Pool low on connections::Total:${totalCount},Idle:${idleCount},Waiting:${waitingCount}`);
+  }
+});
+
 function promiseQuery (queryString, params) {
   return new Promise((resolve, reject) => {
     query(queryString, params, (res) => {
@@ -23,8 +30,6 @@ function query (queryString, params, cb) {
 }
 
 module.exports = {
-
   query: promiseQuery,
   pool
-
 };

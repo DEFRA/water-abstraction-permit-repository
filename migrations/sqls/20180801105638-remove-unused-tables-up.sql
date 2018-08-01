c3993bedabc9093908359f38a487787e7e3456aa
Mark Swaffer <mdswaffer@gmail.com>
2018-08-01 12:17:55 +0100

    Remove unused tables and code
     - Removes some unused tables
     - Removes some unused code that is UI specific.
     - Update packages to fix security warnings.

---
+++ migrations/sqls/20180801105638-remove-unused-tables-up.sql
@@ -0,0 +1,5 @@
+drop table if exists permit.x_licence_shortcode;
+drop table if exists permit.x_user_licence;
+drop table if exists permit.x_users;
+drop table if exists permit.licence_data;
+drop table if exists permit.licence_shortcode;

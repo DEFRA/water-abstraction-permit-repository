2feb32df2bc7cfbddb5e1880866795c7bef18da6
Mark Swaffer <mdswaffer@gmail.com>
2018-11-22 11:39:30 +0000

    Feat: Consolidate env variables (#56)
    WATER-1685
    
    - Removes cache key from README (not used)
    - Use upper case env variable keys.
    - Adds example env file.
    - Remove errbit vars from travis builds.

--- .labrc.js
+++ .labrc.js
@@ -3,5 +3,12 @@
 // This is overridden if arguments are passed to lab via the command line.
 module.exports = {
   globals: 'fetch,Response,Headers,Request',
-  verbose: true
+  verbose: true,
+  'coverage-exclude': [
+    'migrations',
+    'node_modules',
+    'scripts',
+    'src/lib/logger/vendor',
+    'test'
+  ]
 };

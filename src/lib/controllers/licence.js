39aa4b32d9a28f501a03abec0981a4ab675bf40d
James Carmichael <james@siteclick.co.uk>
2018-12-18 14:00:27 +0000

    Reduce controller complexity

--- src/lib/controllers/licence.js
+++ src/lib/controllers/licence.js
@@ -3,6 +3,29 @@ const Joi = require('joi');
 const { reduceGridReferenceResolution, isWaterAbstractionLicence } = require('../helpers.js');
 const deepMap = require('deep-map');
 
+/**
+ * Post select handler automatically searches for and reduces the resolution
+ * of NGR grid references down to 6 figures
+ * @param  {Array} data - rows from licences table
+ * @return {Array}      - rows with licence_data_value NGRs reduced in resolution
+ */
+const postSelect = (data) => {
+  return data.map(row => {
+    if (!isWaterAbstractionLicence(row)) {
+      return row;
+    }
+
+    const { licence_data_value: licenceDataValue, ...rest } = row;
+
+    // Filter out grid refs when licence data added with deep-map
+    const filtered = deepMap(licenceDataValue, (val) => {
+      return typeof (val) === 'string' ? reduceGridReferenceResolution(val) : val;
+    });
+
+    return { licence_data_value: filtered, ...rest };
+  });
+};
+
 module.exports = (config = {}) => {
   const { pool, version } = config;
   return new HAPIRestAPI({
@@ -16,22 +39,7 @@ module.exports = (config = {}) => {
       fields: ['licence_regime_id', 'licence_type_id', 'licence_ref'],
       set: ['licence_status_id', 'licence_search_key', 'is_public_domain', 'licence_start_dt', 'licence_end_dt', 'licence_data_value']
     },
-    postSelect: (data) => {
-      return data.map(row => {
-        if (!isWaterAbstractionLicence(row)) {
-          return row;
-        }
-
-        const { licence_data_value: licenceDataValue, ...rest } = row;
-
-        // Filter out grid refs when licence data added with deep-map
-        const filtered = deepMap(licenceDataValue, (val) => {
-          return typeof (val) === 'string' ? reduceGridReferenceResolution(val) : val;
-        });
-
-        return { licence_data_value: filtered, ...rest };
-      });
-    },
+    postSelect,
     validation: {
       licence_status_id: Joi.number(),
       licence_type_id: Joi.number(),

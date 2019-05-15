9b2e741f4741d1be5177461535bab5bcea8ea532
Mark Swaffer <mdswaffer@gmail.com>
2019-05-15 12:11:40 +0100

    Chore: Update eslint and add ignore file (#130)

--- scripts/create-schema.js
+++ scripts/create-schema.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { pool } = require('../src/lib/connectors/db');
 
 async function run () {
-  const {error} = await pool.query('CREATE SCHEMA IF NOT EXISTS permit;');
+  const { error } = await pool.query('CREATE SCHEMA IF NOT EXISTS permit;');
   console.log(error || 'OK');
   process.exit(error ? 1 : 0);
 }
--- src/routes/API.js
+++ src/routes/API.js
@@ -5,15 +5,15 @@ API page, pending real back end - uses fs to read and write to lkocal json files
 // const API = require('../lib/API');
 const version = '1.0';
 
-const {pool} = require('../lib/connectors/db.js');
+const { pool } = require('../lib/connectors/db.js');
 
-const {RegimeApi, LicenceTypeApi, LicenceApi, LicenceExpiringApi} = require('../lib/controllers')({pool, version});
+const { RegimeApi, LicenceTypeApi, LicenceApi, LicenceExpiringApi } = require('../lib/controllers')({ pool, version });
 
 var licenceRoutes = LicenceApi.getRoutes();
-licenceRoutes[2].config.payload = {maxBytes: 10485760};
+licenceRoutes[2].config.payload = { maxBytes: 10485760 };
 
 module.exports = [
-  { method: 'GET', path: '/status', handler: function (request, h) { return h.response('ok').code(200); }, config: {auth: false, description: 'Get all entities'} },
+  { method: 'GET', path: '/status', handler: function (request, h) { return h.response('ok').code(200); }, config: { auth: false, description: 'Get all entities' } },
   ...RegimeApi.getRoutes(),
   ...LicenceTypeApi.getRoutes(),
   ...LicenceExpiringApi.getRoutes(),

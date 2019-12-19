4682eaf38c1220d68057a81dab081a768654ccbb
James Carmichael <james@siteclick.co.uk>
2019-12-19 14:35:19 +0000

    Use db.createPool() from water-abstraction-helpers

--- src/lib/connectors/db.js
+++ src/lib/connectors/db.js
@@ -1,14 +1,7 @@
-const { pg } = require('../../../config');
-const { Pool } = require('pg');
+require('dotenv').config();
+const helpers = require('@envage/water-abstraction-helpers');
+
+const config = require('../../../config.js');
 const { logger } = require('../../logger');
 
-const pool = new Pool(pg);
-
-pool.on('acquire', () => {
-  const { totalCount, idleCount, waitingCount } = pool;
-  if (totalCount === pg.max && idleCount === 0 && waitingCount > 0) {
-    logger.info(`Pool low on connections::Total:${totalCount},Idle:${idleCount},Waiting:${waitingCount}`);
-  }
-});
-
-exports.pool = pool;
+exports.pool = helpers.db.createPool(config.pg, logger);

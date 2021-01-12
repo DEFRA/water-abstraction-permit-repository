8779d5ef8f860d50506f1f4bd0166d3e7a51357b
Tim <tim@tamimimail.com>
2021-01-12 08:29:11 +0000

    Release/charging sprint 38 (#238)
    * Add new CI (#228)
    
    - Replaces Travis CI with GitHub actions
    - Replaces CodeClimate checks with SonarCloud for code smells
    - Keeps CodeClimate for coverage metrics.
    
    * Bump codecov from 3.7.2 to 3.8.1 (#227)
    
    Bumps [codecov](https://github.com/codecov/codecov-node) from 3.7.2 to 3.8.1.
    - [Release notes](https://github.com/codecov/codecov-node/releases)
    - [Changelog](https://github.com/codecov/codecov-node/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/codecov/codecov-node/compare/v3.7.2...v3.8.1)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Tim <tim@tamimimail.com>
    
    * Update README.MD
    
    * Reconfig
    
    * Update ecosystem.config.json
    
    * Update config.js
    
    Changed config to 6 max connections (was 4)
    
    * Update config.js
    
    * 1.59.0
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const testMode = parseInt(process.env.TEST_MODE) === 1;
-const isAcceptanceTestTarget = ['local', 'dev', 'development', 'test', 'preprod'].includes(process.env.NODE_ENV);
+const isAcceptanceTestTarget = ['local', 'dev', 'development', 'test', 'qa', 'preprod'].includes(process.env.NODE_ENV);
 
 module.exports = {
 
@@ -19,31 +19,9 @@ module.exports = {
     airbrakeLevel: 'error'
   },
 
-  // Database has 198 available connections
-  //
-  // Outside of development each process runs on 2 instances on 2 cores.
-  // So there will be 4 connection pools per service but just 1 locally
-  //
-  // Allocations:
-  //
-  // | ----------------------------------- | ------- | ---------- | --------- | ---------- |
-  // | Service                             | Local   | Local      | Non local | Non local  |
-  // |                                     | process | connection | process   | connection |
-  // |                                     | count   | count      | count     | count      |
-  // | ----------------------------------- | ------- | ---------- | --------- | ---------- |
-  // | water-abstraction-import            |       1 |         20 |         2 | (20)    10 |
-  // | water-abstraction-permit-repository |       1 |         16 |         4 | (16)     4 |
-  // | water-abstraction-returns           |       1 |         20 |         4 | (20)     5 |
-  // | water-abstraction-service           |       2 | (100)   50 |         5 | (100)   20 |
-  // | water-abstraction-tactical-crm      |       1 |         20 |         4 | (20)     5 |
-  // | water-abstraction-tactical-idm      |       1 |         20 |         4 | (20)     5 |
-  // | ----------------------------------- | ------- | ---------- | --------- | ---------- |
-  // | TOTAL                               |       6 |        196 |        23 |        196 |
-  // | ----------------------------------- | ------- | ---------- | --------- | ---------- |
-  //
   pg: {
     connectionString: process.env.DATABASE_URL,
-    max: process.env.NODE_ENV === 'local' ? 16 : 4,
+    max: process.env.NODE_ENV === 'local' ? 16 : 6,
     idleTimeoutMillis: 10000,
     connectionTimeoutMillis: 5000
   },

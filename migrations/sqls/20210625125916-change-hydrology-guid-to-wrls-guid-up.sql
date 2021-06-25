f60fbc861aa786a1809d77d1f287a7d8436a484d
Tim <tim@tamimi.dev>
2021-06-25 14:10:41 +0100

    Adds migration

---
+++ migrations/sqls/20210625125916-change-hydrology-guid-to-wrls-guid-up.sql
@@ -0,0 +1,17 @@
+do $$ declare gsRow record;
+begin for gsRow in (
+select
+hydrology_station_id as oldguid,
+gauging_station_id as newguid
+from
+water.gauging_stations) loop
+update
+permit.licence
+set
+licence_data_value = replace( licence_data_value::text, gsRow.oldguid::text, gsRow.newguid::text )::jsonb
+where
+licence_type_id = 10
+and licence_data_value::text ilike '%' || gsRow.oldguid::text || '%';
+end loop;
+end;
+$$

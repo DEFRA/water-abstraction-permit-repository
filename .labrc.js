459035ae72ed84271579e2cbeb00a911dc16c0e1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-06-20 09:37:46 +0100

    Add silent skips as a default to labrc

--- .labrc.js
+++ .labrc.js
@@ -5,6 +5,9 @@
 module.exports = {
   verbose: true,
   coverage: true,
+  // Menas when we use *.only() in our tests we just get the output for what we've flagged rather than all output but
+  // greyed out to show it was skipped
+  'silent-skips': true,
   // lcov reporter required for SonarCloud
   reporter: ['console', 'html', 'lcov'],
   output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info'],

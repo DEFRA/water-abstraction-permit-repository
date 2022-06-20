246e738815c1f908825eb37af02c01924ccc9967
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-06-20 01:23:33 +0100

    Apply consistent CI workflow (#275)
    https://github.com/DEFRA/water-abstraction-team/issues/4
    
    As a team, we are currently maintaining 11 repos and that's not including documentation, support and deployment-related stuff! What we inherited was not consistent; different CI tools were used for the same purpose and different CI steps were implemented.
    
    An effort was made to get this under control with [water-abstraction-orchestration](https://github.com/DEFRA/water-abstraction-orchestration). But to get a handle on it in such a way as the current team can manage, we're simplifying still further.
    
    This change updates `.github/workflows/ci.yml` to use a template we're applying across all the repos, amended to the requirements of the project. In the future, we hope to return to the lessons **water-abstraction-orchestration** taught us, if only to remove the duplication across the projects. But for now, the template hopefully simplifies and makes consistent our build process.
    
    ** Notes
    
    - Apply ci.yml template
    
    This is very much based on https://github.com/DEFRA/sroc-charging-module-api/blob/main/.github/workflows/ci.yml
    
    There is now one process, which means you'll just see the one item listed in the checks on GitHub. The steps are ordered in an effort to fail fast and/or where needed, for example, DB migrations before running tests and SonarCloud analysis after code coverage data has been generated.
    
    - Update .labrc
    
    This is based on https://github.com/DEFRA/sroc-charging-module-api/blob/main/.labrc.js. We use the config file to ensure we generate the coverage output in a format needed by SonarCloud.
    
    - Add SonarCloud properties file
    
    Previously, the CI specified the values SonarCloud needs in its steps plus there were some in the web UI. But there are a bunch of other things we can tell SonarCloud about which makes managing SonarCloud much easier.
    
    So, we use `sonar-project.properties` files to set what the SonarCloud analysis step needs and what's useful in its UI.
    
    - Update README with new badges
    
    Also displays the badges we want in a layout that is consistent with most open source repos.

--- .labrc.js
+++ .labrc.js
@@ -1,13 +1,12 @@
+'use strict'
 // default settings for lab test runs.
 //
 // This is overridden if arguments are passed to lab via the command line.
 module.exports = {
-  globals: 'fetch,Response,Headers,Request',
   verbose: true,
-  'coverage-exclude': [
-    'migrations',
-    'node_modules',
-    'scripts',
-    'test'
-  ]
+  coverage: true,
+  // lcov reporter required for SonarCloud
+  reporter: ['console', 'html', 'lcov'],
+  output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info'],
+  globals: ['fetch','Response','Headers','Request'].join(',')
 };
